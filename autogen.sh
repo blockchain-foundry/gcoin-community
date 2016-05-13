8fadf56f655179b479247fc987505a2a68ed6a0b
En-Ran Zhou <zhouer@gmail.com>
2016-05-13 15:29:24 -0500

    update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -e
 srcdir="$(dirname $0)"
-( cd "$srcdir"
+cd "$srcdir"
 if [ -z ${LIBTOOLIZE} ] && GLIBTOOLIZE="`which glibtoolize 2>/dev/null`"; then
   LIBTOOLIZE="${GLIBTOOLIZE}"
   export LIBTOOLIZE
 fi
-autoreconf --install --force --warnings=all ) && "$srcdir/configure" "$@"
+which autoreconf >/dev/null || \
+  (echo "configuration failed, please install autoconf first" && exit 1)
+autoreconf --install --force --warnings=all
